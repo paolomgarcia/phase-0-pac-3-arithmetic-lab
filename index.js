75f9715116a90a7edd481f878254c6165b8e3675
Paolo Garcia <pmatteo1108@yahoo.com>
2022-08-31 18:20:43 -0600

    commit tree 69d5496b9bc6bae9f5d6ad8029b0fc7084abfbe5

---
+++ index.js
@@ -0,0 +1,51 @@
+function add() {   
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+  function add(a, b) {
+    return a + b;
+  }
+
+  function subtract (a, b) {
+    return a-b;
+  }
+
+  function multiply (a, b) {
+    return a*b;
+  }
+
+  function divide(a,b) {
+    return a/b;
+  }
+
+  function increment(n) {
+    n++;
+    return n;
+  }
+ 
+  function decrement(n) {
+    n--;
+    return n;
+  }
+
+function makeInt(s) {
+    return parseInt(s, 10);
+}
+
+function preserveDecimal(s) {
+    return parseFloat(s);
+}
+
+
